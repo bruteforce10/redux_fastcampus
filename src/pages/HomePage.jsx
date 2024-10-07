a72339aa2bf117eb58a3d59e74d54a4c98cffdb7
Firdi Audi Putra <39665978+bruteforce10@users.noreply.github.com>
2024-10-07 18:08:18 +0700

    fetch update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,24 +1,11 @@
 import ProductCard from "@/components/ProductCard";
-
-const productRaw = [
-  {
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkiGMULsAHa9dID44BozhTQWOlhmMY7a11nQ&s",
-    productName: "Product 1",
-    price: 1000,
-    stock: 0,
-  },
-  {
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkiGMULsAHa9dID44BozhTQWOlhmMY7a11nQ&s",
-    productName: "Product 2",
-    price: 3000,
-    stock: 10,
-  },
-];
+import { axiosInstance } from "@/lib/axios";
+import { useState } from "react";
 
 const HomePage = () => {
-  const products = productRaw.map((product) => (
+  const [products, setProducts] = useState([]);
+
+  const productsList = products.map((product) => (
     <ProductCard
       key={product.productName}
       image={product.image}
@@ -28,10 +15,22 @@ const HomePage = () => {
     />
   ));
 
+  const fetchProducts = async () => {
+    try {
+      const res = await axiosInstance.get("/products");
+
+      setProducts(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <main className="min-h-[80vh] max-w-screen-md mx-auto px-4 mt-8">
-        <div className="grid grid-cols-2 gap-4">{products}</div>
+        <div className="grid grid-cols-2 gap-4">{productsList}</div>
+
+        <button onClick={fetchProducts}>Fetch Products</button>
       </main>
     </>
   );
